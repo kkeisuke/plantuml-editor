e21359171eb275760a9207c51095036111ea7e43
kimura keisuke <kimura@n-create.co.jp>
2017-03-14 01:37:46 +0900

    開発環境のルートパスまで書き換えてしまっていたので修正

--- config/index.js
+++ config/index.js
@@ -26,7 +26,7 @@ module.exports = {
     port: 8080,
     autoOpenBrowser: true,
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/plantuml-editor/',
+    assetsPublicPath: '/',
     proxyTable: {},
     // CSS Sourcemaps off by default because relative paths are "buggy"
     // with this option, according to the CSS-Loader README
