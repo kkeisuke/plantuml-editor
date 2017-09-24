2f62ac3a3fb362972c571b131a8ab0b8bb00fcab
kimura keisuke <mail@kkeisuke.com>
2017-09-25 06:35:22 +0900

    非同期処理を actions へ移行

--- src/store/modules/GistApi.js
+++ src/store/modules/GistApi.js
@@ -49,59 +49,17 @@ const mutations: any = {
   resetFiles () {
     state.gist.files = {}
   },
-  addTxtUML (state: any, data: any) {
-    if (data) {
-      data.ext = state.ext.txt
-      mutations.addFiles(state, data)
-    }
-  },
-  addSvgUML (state: any, data: any): any {
-    return axios.get(data.src || '')
-    .then((response: any) => {
-      if (response && response.data) {
-        data.ext = state.ext.svg
-        data.text = response.data
-        mutations.addFiles(state, data)
-      }
-    })
-  },
-  createGist (state: any, data: any) {
-    // 送信中フラグ ON
+  startSending (state: any) {
     state.isSending = true
-
-    // リセット
-    mutations.resetResponse()
-    mutations.resetFiles()
-
-    // テキスト追加
-    mutations.addTxtUML(state, data)
-
-    // svg 再取得
-    axios.all([mutations.addSvgUML(state, data)])
-    .then(axios.spread(() => {
-      // text と svg が両方ある場合
-      if (Object.keys(state.gist.files).length >= 2) {
-        // Gist 投稿
-        axios.post(`${state.api}?access_token=${state.token}`, state.gist)
-        .then((response: any) => {
-          state.gistUrl = response.data.html_url
-          state.isSending = false
-        })
-        .catch((error: any) => {
-          state.errorMsg = `${error.response.status} ${error.response.statusText} ${error.response.data.message}`
-          state.isSending = false
-        })
-      } else {
-        state.errorMsg = 'could not get SVG'
-        state.isSending = false
-      }
-    }))
   },
-  resetGist () {
-    mutations.setDescription(state, '')
-    mutations.setPublic(state, true)
-    mutations.resetFiles()
-    mutations.resetResponse()
+  stopSending (state: any) {
+    state.isSending = false
+  },
+  setGistURL (state: any, url: string) {
+    state.gistUrl = url
+  },
+  setErrorMsg (state: any, errorMsg: string) {
+    state.errorMsg = errorMsg
   },
   resetResponse () {
     state.gistUrl = ''
@@ -119,11 +77,59 @@ const actions: any = {
   setPublic (context: any, isPublic: boolean) {
     context.commit('setPublic', isPublic)
   },
-  createGist (context: any, data: any) {
-    context.commit('createGist', data)
+  addTxtUML ({state, commit}: any, data: any) {
+    if (data) {
+      data.ext = state.ext.txt
+      commit('addFiles', data)
+    }
   },
-  resetGist (context: any) {
-    context.commit('resetGist')
+  addSvgUML ({state, commit}: any, data: any): any {
+    return axios.get(data.src || '')
+    .then((response: any) => {
+      if (response && response.data) {
+        data.ext = state.ext.svg
+        data.text = response.data
+        commit('addFiles', data)
+      }
+    })
+  },
+  createGist ({state, commit, dispatch}: any, data: any) {
+    // 送信中フラグ ON
+    commit('startSending')
+
+    // リセット
+    commit('resetResponse')
+    commit('resetFiles')
+
+    // テキスト追加
+    dispatch('addTxtUML', data)
+
+    // svg 再取得
+    axios.all([dispatch('addSvgUML', data)])
+    .then(axios.spread(() => {
+      // text と svg が両方ある場合
+      if (Object.keys(state.gist.files).length >= 2) {
+        // Gist 投稿
+        axios.post(`${state.api}?access_token=${state.token}`, state.gist)
+        .then((response: any) => {
+          commit('setGistURL', response.data.html_url)
+          commit('stopSending')
+        })
+        .catch((error: any) => {
+          commit('setErrorMsg', `${error.response.status} ${error.response.statusText} ${error.response.data.message}`)
+          commit('stopSending')
+        })
+      } else {
+        commit('setErrorMsg', 'could not get SVG')
+        commit('stopSending')
+      }
+    }))
+  },
+  resetGist ({commit}: any) {
+    commit('setDescription', '')
+    commit('setPublic', true)
+    commit('resetFiles')
+    commit('resetResponse')
   }
 }
 
