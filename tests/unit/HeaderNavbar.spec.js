c17fff7d76037bfcb56cae2c28e4a7f93a752e0d
kimura keisuke <mail@kkeisuke.com>
2018-05-10 15:52:16 +0900

    テストのマイグレーション
    - spec.js の Jest API への書き換え

--- tests/unit/HeaderNavbar.spec.js
+++ tests/unit/HeaderNavbar.spec.js
@@ -5,27 +5,27 @@ import HeaderNavbar from '@/components/HeaderNavbar'
 describe('HeaderNavbar.vue', () => {
   let vm: any = null
 
-  before(() => {
+  beforeEach(() => {
     const Constructor: any = Vue.extend(HeaderNavbar)
     vm = new Constructor({store}).$mount()
   })
 
   describe('methods changeHistoryColSize', () => {
-    before(() => {
+    beforeEach(() => {
       vm.$store.dispatch('layout/setEditColSize')
     })
 
     it('state.layout.colSize.history', () => {
-      expect(vm.$store.state.layout.colSize.history).to.equal(0)
+      expect(vm.$store.state.layout.colSize.history).toEqual(0)
     })
     it('state.layout.colSize.editor', () => {
-      expect(vm.$store.state.layout.colSize.editor).to.equal(5)
+      expect(vm.$store.state.layout.colSize.editor).toEqual(5)
     })
     it('state.layout.colSize.cheatSheet', () => {
-      expect(vm.$store.state.layout.colSize.cheatSheet).to.equal(0)
+      expect(vm.$store.state.layout.colSize.cheatSheet).toEqual(0)
     })
     it('state.layout.colSize.uml', () => {
-      expect(vm.$store.state.layout.colSize.uml).to.equal(7)
+      expect(vm.$store.state.layout.colSize.uml).toEqual(7)
     })
   })
 })
